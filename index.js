d67ecb1eb10019ad6b387122c50e64b38f02735e
lobana alarabi <loubanaeng@gmail.com>
2020-01-29 05:07:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var driver1= {a:'20', b: '30'}
+function updateDriverWithKeyAndValue(driver, key, value)
+{
+  const driver={};
+  driver[key]=value;
+  return driver;
+}
+
+ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value)
+ {
+   
+ }
+ 
+ function deleteFromDriverByKey(driver, key)
+ {
+   
+ }
+ 
+ function destructivelyDeleteFromDriverByKey(driver, key)
+ {
+   
+ }
+ 
