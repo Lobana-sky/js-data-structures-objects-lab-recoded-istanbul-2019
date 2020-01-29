95788e4a35234373cf08681bea89bfc49a9207b3
lobana alarabi <loubanaeng@gmail.com>
2020-01-29 05:09:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var driver1= {a:'20', b: '30'}
 function updateDriverWithKeyAndValue(driver, key, value)
 {
-  
+  const newDriver=object.assign({},driver);
   driver[key]=value;
   return driver;
 }
